8ef7ca22138c83a561fee95fe1866cedfd97a160
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2025-12-06 02:15:22 +0000

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,45 @@
-with open("day1/input.txt") as f:
-    lines = [line.strip() for line in f]
+def part1():
 
-print(lines)
+    with open("day1/input.txt") as f:
+        lines = [line.strip() for line in f]
+
+    pos = 50
+    count = 0
+
+    for line in lines:
+        dir = line[0]
+        val = int(line[1:])
+
+        if dir == "R": pos += val
+        else: pos -= val
+
+        pos %= 100
+
+        if pos == 0: count += 1
+
+    print(count)
+
+def part2():
+
+    with open("day1/input.txt") as f:
+        lines = [line.strip() for line in f]
+
+    pos = 50
+    count = 0
+
+    for line in lines:
+        dir = line[0]
+        val = int(line[1:])
+
+        step = -1 if dir == 'L' else +1
+
+        for _ in range(val):
+            pos = (pos + step) % 100
+            if pos == 0:
+                count += 1
+
+    print(count)
+
+
+part1()
+part2()
